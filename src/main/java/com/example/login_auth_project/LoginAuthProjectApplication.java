614ee37fd08f5cf9a898206b2f79d41a1b7cdf8f
Matheus.lima <limamatheus0010@gmail.com>
2024-09-11 23:32:12 -0300

    Projeto Base

---
+++ src/main/java/com/example/login_auth_project/LoginAuthProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.login_auth_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAuthProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAuthProjectApplication.class, args);
+	}
+
+}
